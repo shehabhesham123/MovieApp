6ca078e838b652016889ae50bff1e0d06328811f
_shehab <54242265+shehabhesham123@users.noreply.github.com>
2024-11-26 01:25:46 +0200

    Create initial movie detials screen

--- app/src/main/java/com/example/movieapp/features/movies/app/viewmodel/viewmodel/MovieDetailsViewModel.kt
+++ app/src/main/java/com/example/movieapp/features/movies/app/viewmodel/viewmodel/MovieDetailsViewModel.kt
@@ -1,11 +1,31 @@
 package com.example.movieapp.features.movies.app.viewmodel.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.viewModelScope
 import com.example.movieapp.core.platform.BasicViewModel
+import com.example.movieapp.features.movies.app.viewmodel.model.MovieDetails
 import com.example.movieapp.features.movies.domain.usecase.GetMovieDetails
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MovieDetailsViewModel @Inject constructor(private val getMovieDetails: GetMovieDetails) :
     BasicViewModel() {
+    var movieDetails by mutableStateOf<MovieDetails?>(null)
+        private set
+
+    fun getMovieDetails(movieId: Int) {
+        viewModelScope.launch {
+            getMovieDetails.invoke(movieId, this) {
+                it.fold(::handleFailure, ::handleMovieDetails)
+            }
+        }
+    }
+
+    fun handleMovieDetails(movieDetails: MovieDetails) {
+        this.movieDetails = movieDetails
+    }
 }
